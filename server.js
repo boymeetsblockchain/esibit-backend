5906bc14086a8678ea2e3c4049a3447caa887bc8
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2024-09-05 18:33:56 +0100

    ready for use on the frontend

--- server.js
+++ server.js
@@ -4,16 +4,24 @@ const dotenv = require('dotenv').config();
 const db = require('./db/db');
 const userRoute = require('./routes/userRoute');
 const doctorRoute = require('./routes/doctorRoute')
-
+const cors = require("cors")
 db();
+
+
+const allowedOrigins = ['http://localhost:3000', 'https://www.esibiti.com'];
+
 
 // Middleware to parse JSON and URL-encoded payloads
 app.use(express.json());
+app.use(cors({ credentials: true, origin: allowedOrigins }));
+
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/user', userRoute);
 app.use('/api/doctor', doctorRoute);
+
+
 
 
 app.get('/', (req,res)=>{
